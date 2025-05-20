902bbfc0132e2dfbeab076f15c3242702158e194
hadinnazmi21 <tengkumuhammad23ti@mahasiswa.pcr.ac.id>
2025-05-20 09:51:24 +0700

    "halooo"

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Haloo gess</h1>
+      <h1>Haloo gesssss</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
